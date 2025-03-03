57b1ecf359ff136025b58b9d78ba57581df047f8
radjabov_sherzod <155184029+rajabovsherzod@users.noreply.github.com>
2025-03-03 17:44:18 +0500

    second commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const person = {
+    name: 'John Doe',
+    age: 30,
+    occupation: 'Software Engineer',
+    address: {
+        street: '123 Main St',
+        city: 'Anytown',
+        state: 'CA',
+    }
+}
+console.log(person.name)
